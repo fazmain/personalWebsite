24c6244ef966de15f2188e8cccec7c1695e96fa3
fazmain <faiaz.azmain99@gmail.com>
2023-12-13 13:12:01 +0600

    Change footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Box, Text, Link, Center } from "@chakra-ui/react";
+import { FaHeart } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <Box as="footer" role="contentinfo" p={4} mt="10%">
       <Center>
         <Text fontSize="sm">
-          © {new Date().getFullYear()}, Built by Faiaz. All rights reserved.
+          © {new Date().getFullYear()}, Built by Faiaz using React and ChakraUI. All rights reserved.
         </Text>
       </Center>
     </Box>
   );
 };
+
 
 export default Footer;
